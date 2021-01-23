4574d625754a416d3e70e83217d7bff6ff2e0981
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-01-23 17:35:52 -0500

    Update app.js

--- belly_button_biodiversity/static/js/app.js
+++ belly_button_biodiversity/static/js/app.js
@@ -1,13 +1,18 @@
 // Create a dropdown function
 function init() {
+    // select dropdown menu
     var dropdown = d3.select("#selDataset");
-}
-
-// Create a function that populates plots for given 'ID'
-function getPlot(id) {
 
     // Use d3.JSON to fetch data from JSON file
     d3.json("data/samples.json").then((data)=> {
-        console.log(data)
-    }
+        sampleData.names.forEach((sample)=> {
+            dropdown.append("option").text(sample.property("value", sample);
+        });
+        // Call variable and Console log data
+        console.log(sampleID);
+        console.log(samplesData);
+        createBarChart(sampleID);
+        createBubbleChart(sampleID);
+        displayMetadata(sampleID);
+    });
 }
