ff425c915634cf0fb11091340ec87d8f57864479
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-01-23 17:50:23 -0500

    Update app.js

--- belly_button_biodiversity/static/js/app.js
+++ belly_button_biodiversity/static/js/app.js
@@ -25,3 +25,20 @@ function optionChnaged(sampleID) {
     createBubbleChart(sampleID);
     displayMetadata(sampleID);
 }
+
+// Create Bar plot
+function createBarChart(selectedID ) {
+    console.log(selectedID.toString());
+    // Use d3.JSON to fetch data from JSON file
+    d3.json("data/samples.json").then((data) => {
+        var samples = data.samples;
+        var selectedData = samples.filter(object => object.id. toString() == selectedID)[0];
+        // Console log selected data
+        console.log(selectedData);
+
+        // Retrieve first 25 of sample values 
+        var revValues = selectedData.sample_values.slice(0,25).reverse();
+        var otuIDs = selectedData.otu_ids.slice(0,25).reverse();
+        var strRevIDs = otuIDs.map(row => "OTU " + row.toString());
+        var revLabels = selectedData.otu_labels.slice(0,25).reverse();
+}
